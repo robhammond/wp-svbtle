ba116a5a90466ba4d6fda813dd6524d347ca582c
Rob Hammond <robbiehammond@gmail.com>
2013-01-31 16:57:52 +0000

    More SEO friendly & interesting 404 page

--- wp-content/themes/svbtle/404.php
+++ wp-content/themes/svbtle/404.php
@@ -7,5 +7,43 @@
  * @since Boilerplate 1.0
  */
 
-wp_redirect(get_bloginfo('url').'?not_found=1',302);
-exit;
+get_header(); ?>
+
+	<div id="primary">
+		<div id="content" role="main">
+
+			<article id="post-0" class="post error404 not-found">
+				<img src="<?php bloginfo('template_directory'); ?>/images/hypnotoad.gif">
+				<header class="entry-header">
+					<h1 class="entry-title">ALL GLORY TO THE HYPNOTOAD!</h1>
+				</header>
+
+				<div class="entry-content">
+					<p>Everybody loves Hypnotoad. If you were looking for something else, it's not here.</p>
+
+					<?php get_search_form(); ?>
+
+					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
+
+					<div class="widget">
+						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'wp-svbtle' ); ?></h2>
+						<ul>
+						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
+						</ul>
+					</div>
+
+					<?php
+					/* translators: %1$s: smilie */
+					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'wp-svbtle' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ), array( 'after_title' => '</h2>'.$archive_content ) );
+					?>
+
+					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
